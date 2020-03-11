c49ad5b5abd4512b4308079d4ede3118cc4fa28b
Incredible Admiral <karman.godlike@gmail.com>
2020-03-11 17:13:46 +0300

    sds

---
+++ RadioStation/View/TPP55Window.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RadioStation.View
+{
+    /// <summary>
+    /// Логика взаимодействия для TPP55Window.xaml
+    /// </summary>
+    public partial class TPP55Window : Window
+    {
+        public TPP55Window()
+        {
+            InitializeComponent();
+        }
+    }
+}
